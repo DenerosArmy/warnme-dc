a410b0c75565ba0f0f641d15bd10e63399574894
Richie Zeng <richzeng@gmail.com>
2012-02-03 23:33:17 -0800

    set up urls

--- main/views.py
+++ main/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def home(request):
+    html = "<html><body>This is the home page</body></html>"
+    return HttpResponse(html)
+
+def food(request):
+    html = "<html><body>This is the list of food</body></html>"
+    return HttpResponse(html)
+
+def food_profile(request,num):
+    html = "<html><body>This is the information about food with id %s</body></html>" %(num)
+    return HttpResponse(html)
+
+def user_profile(request,num):
+    html = "<html><body>This is the information about user with id %s</body></html>" %(num)
+    return HttpResponse(html)
