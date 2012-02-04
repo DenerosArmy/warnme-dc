dfb2b7079fd845d994fd2c82ad1b9a71f638e2c1
Nikita Kitaev <nikitakit@gmail.com>
2012-02-04 10:40:53 -0800

    Show UserProfile table in admin

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from main.models import FoodTag, Food, UserRating, Offering
+from main.models import FoodTag, Food, UserRating, Offering, UserProfile
 
 admin.site.register(FoodTag)
 admin.site.register(Food)
 admin.site.register(UserRating)
 admin.site.register(Offering)
+admin.site.register(UserProfile)
