72b5275ce054f1a255261d05e2b8d3d9eec74d94
Vaishaal <vaishaal@berkeley.edu>
2012-02-04 00:54:06 -0800

    initial foodlist created

--- menu/menu.py
+++ menu/menu.py
@@ -1,16 +1,19 @@
 import urllib 
 from BeautifulSoup import *
 import subprocess
-""" Gathers data from dining common website and stores results in text file """ 
-BLANK_LENGTH = 10170
-skipped = False 
-skip_count = 0 
-DC = "FOOTHILL"
-PATH = "/Users/vaishaal/Code4Cal/warnme-dc/menu/data/{0}".format(DC)
-working_file = open(PATH + "/food", "w")
-good_file = True
-food_name = "dur" 
-for i in range(231000, 300000):
+import sys
+
+def menu_scrape(x,y,foodn): 
+	""" Gathers data from dining common website and stores results in text file """ 
+	BLANK_LENGTH = 10170
+	skipped = False 
+	skip_count = 0
+	DC = "Foothill" 
+	PATH = "/Users/vaishaal/Code4Cal/warnme-dc/menu/data/{0}".format(DC)
+	good_file = True
+	food_name = "dur" 
+	for i in range(x, y):
+		working_file = open(PATH + "/food{0}".format(foodn), "w")
 		page = urllib.urlopen("http://services.housing.berkeley.edu/FoodPro/dining/static/label.asp?locationNum=06&locationName={0}&dtdate=2%2F3%2F2012&RecNumAndPort={1}".format(DC,i)) 
 		data = page.read() 
 		if len(data) <= 10170 :
@@ -20,6 +23,9 @@ for i in range(231000, 300000):
 		food_name += '\n' 
 		print(food_name) 
 		working_file.write(food_name)
-working_file.close() 
+		working_file.close() 
 
-
+x = int(sys.argv[1])
+y = int(sys.argv[2]
+z = sys.argv[3] 
+menu_scrape(x,y,z) 
