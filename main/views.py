6640c5b0c10af2c7fa98c66524583ba80053143e
Nikita Kitaev <nikitakit@gmail.com>
2012-02-04 05:30:31 -0800

    Display all menu entries in home page

--- main/views.py
+++ main/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+import datetime
 from main.models import *
 
 def home(request):
-    data = {'FH': {'B': { "main_foods": Food.objects.all(),
-                          "other_foods": Food.objects.all() }}}
+    data = {}
+
+    date = datetime.date.today()
+    for location in map(lambda x: x[0], Offering.LOCATION_CHOICES):
+        data[location] = {}
+        if date.isoweekday() != 6 and date.isoweekday() != 7:
+            data[location]['B'] = sorted_foods(date, location, 'B')
+
+        data[location]['L'] = sorted_foods(date, location, 'L')
+        data[location]['D'] = sorted_foods(date, location, 'D')
+
     return render_to_response('home.html', RequestContext(request,{
                 "data": data}))
+
+def sorted_foods(date, location, meal):
+    o = Offering.objects.filter(location=location, date=date, meal=meal)
+    if len(o) == 0:
+        return {}
+    else:
+        return {"main_foods": o[0].foods.order_by("-rating"),
+                "other_foods": []}
 
 def food(request):
     return render_to_response('food.html', RequestContext(request, {}))
