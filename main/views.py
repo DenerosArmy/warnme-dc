50a94b5aeaaabf8e15594a8b71466c170119ccbe
Richie Zeng <richzeng@gmail.com>
2012-02-04 02:34:03 -0800

    updated the views.py

--- main/views.py
+++ main/views.py
@@ -1,18 +1,13 @@
-from django.http import HttpResponse
-import datetime
+from django.shortcuts import render_to_response
 
 def home(request):
-    html = "<html><body>This is the home page</body></html>"
-    return HttpResponse(html)
+    return render_to_response('home.html', {})
 
 def food(request):
-    html = "<html><body>This is the list of food</body></html>"
-    return HttpResponse(html)
+    return render_to_response('food.html', {})
 
 def food_profile(request,num):
-    html = "<html><body>This is the information about food with id %s</body></html>" %(num)
-    return HttpResponse(html)
+    return render_to_response('food_pf.html', {'id': num})
 
 def user_profile(request,num):
-    html = "<html><body>This is the information about user with id %s</body></html>" %(num)
-    return HttpResponse(html)
+    return render_to_response('user.html', {'id': num})
